8a1e7bac64c57e2455245bd55e1a116ef490209e
Ayesh-J <jamadarayesh9@gmail.com>
2025-05-30 16:13:09 +0530

    Fixed Routing and 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import './index.css'
